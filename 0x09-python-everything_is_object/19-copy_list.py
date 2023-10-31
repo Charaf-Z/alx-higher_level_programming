95b1f6a61a5fb47729dd0bfbb5b1b3b52e0e5ca2
Charaf-Z <charafomarzitouni@gmail.com>
2023-10-31 16:39:04 +0100

    upload files

---
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""copy_list module."""
+
+
+def copy_list(l1) -> list:
+    """
+    Create a copy of list.
+
+    Args:
+        l1 (list): The list to be copied.
+
+    Returns:
+        list: A copy of the input list.
+    """
+    return l1.copy()
