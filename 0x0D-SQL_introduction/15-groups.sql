fc2918ba66b0f1888d0d6862a34516f596551bc2
Charaf-Z <charafomarzitouni@gmail.com>
2023-12-12 15:19:47 +0100

    update the comment 15-file

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,4 +1,4 @@
---list the number of records with the same score in the table second_table of the database hbtn_0c_0 in the MySQL server.
+-- list the number of records with the same score in the table second_table of the database hbtn_0c_0 in the MySQL server.
 SELECT `score`, COUNT(*) AS `number`
 FROM `second_table`
 GROUP BY `score`
