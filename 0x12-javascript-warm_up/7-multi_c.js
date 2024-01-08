53c3ab328b9fe180608ecf637d22529e861cf8bb
Charaf-Z <charafomarzitouni@gmail.com>
2024-01-08 16:13:20 +0100

    update the 7-file

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,7 +3,7 @@ let x = Math.floor(Number(process.argv[2]));
 if (isNaN(x)) {
   console.log('Missing number of occurrences');
 } else {
-  while (x) {
+  while (x > 0) {
     console.log('C is fun');
     x--;
   }
